e652e64f66295ea1e94143a5239a0a6b026e2f1f
MiniKlar <omontloris@gmail.com>
2025-10-30 01:05:51 +0100

    - Added ScavTrap class implementation with constructors, destructor, and member functions for attack and guardGate.

---
+++ ex03/ScavTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 14:59:37 by lomont            #+#    #+#             */
+/*   Updated: 2025/10/30 00:19:58 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include "iostream"
+
+class ScavTrap : virtual public ClapTrap
+{
+	public:
+		ScavTrap( void );
+		ScavTrap( std::string setName );
+		ScavTrap( ScavTrap const & src );
+		ScavTrap& operator=( ScavTrap const & other );
+		~ScavTrap( void );
+		void attack(const std::string& target) ;
+		void guardGate( void );
+};
+
+#endif
