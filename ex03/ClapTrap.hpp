a595eb750dddc20bb16f9609b5e48a938a85923a
MiniKlar <omontloris@gmail.com>
2025-10-30 01:05:24 +0100

    - Added ClapTrap class implementation and header file with constructors and member functions

---
+++ ex03/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 12:03:11 by lomont            #+#    #+#             */
+/*   Updated: 2025/10/30 00:58:26 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+#include "iostream"
+
+class ClapTrap
+{
+	protected:
+		std::string 			Name;
+		unsigned int			HitPoints;
+		unsigned int			EnergyPoints;
+		unsigned int			AttackDamage;
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string setName );
+		ClapTrap( ClapTrap const & src );
+		ClapTrap& operator=( ClapTrap const & other );
+		~ClapTrap( void );
+		void attack( const std::string& target );
+		void takeDamage( unsigned int amount );
+		void beRepaired( unsigned int amount );
+};
+
+#endif
