0f633aa7b925db7ded6280f4471d9f722a5f3542
MiniKlar <omontloris@gmail.com>
2025-10-30 01:05:00 +0100

    - Refactored ClapTrap constructors for clarity; updated default constructor and removed unused parameters.

--- ex02/ClapTrap.hpp
+++ ex02/ClapTrap.hpp
@@ -6,17 +6,16 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:03:11 by lomont            #+#    #+#             */
-/*   Updated: 2025/10/21 02:27:54 by lomont           ###   ########.fr       */
+/*   Updated: 2025/10/30 00:00:45 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #pragma once
-
-#include "iostream"
 
 #ifndef CLAPTRAP_H
 #define CLAPTRAP_H
+
+#include "iostream"
 
 class ClapTrap
 {
@@ -26,8 +25,8 @@ class ClapTrap
 		unsigned int			EnergyPoints;
 		unsigned int			AttackDamage;
 	public:
+		ClapTrap( void );
 		ClapTrap( std::string setName );
-		ClapTrap( std::string setName, int setHitPoints, int setEnergyPoints, int setAttackDamage );
 		ClapTrap( ClapTrap const & src );
 		ClapTrap& operator=( ClapTrap const & other );
 		~ClapTrap( void );
