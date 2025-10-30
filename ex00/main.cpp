c9b89c50967e2e7884f1a4124a083c8a404b2d34
MiniKlar <omontloris@gmail.com>
2025-10-30 01:04:26 +0100

    - Updated main function to reflect ClapTrap behavior with hit points and energy points, including adjustments to comments for clarity.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:03:12 by lomont            #+#    #+#             */
-/*   Updated: 2025/10/15 13:03:16 by lomont           ###   ########.fr       */
+/*   Updated: 2025/10/29 23:49:56 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,25 @@ int main(void)
 	clap.takeDamage(5);
 	clap.beRepaired(3);
 	clap.takeDamage(8);
-	clap.takeDamage(1); // Should do nothing if HP is 0
+	clap.takeDamage(1);
 
 	std::cout << "\n--- NO HIT POINTS LEFT ---" << std::endl;
-	clap.attack("another bandit"); // Cannot attack
+	clap.attack("another bandit");
 
 	ClapTrap trap("TR4P");
 	std::cout << "\n--- NO ENERGY POINTS LEFT ---" << std::endl;
-	trap.beRepaired(1); // EP: 9
-	trap.beRepaired(1); // EP: 8
-	trap.beRepaired(1); // EP: 7
-	trap.beRepaired(1); // EP: 6
-	trap.beRepaired(1); // EP: 5
-	trap.attack("skag"); // EP: 4
-	trap.attack("skag"); // EP: 3
-	trap.attack("skag"); // EP: 2
-	trap.attack("skag"); // EP: 1
-	trap.attack("skag"); // EP: 0
-	trap.attack("skag"); // No energy left
-	trap.beRepaired(1);  // No energy left
+	trap.beRepaired(1);
+	trap.beRepaired(1);
+	trap.beRepaired(1);
+	trap.beRepaired(1);
+	trap.beRepaired(1);
+	trap.attack("skag");
+	trap.attack("skag");
+	trap.attack("skag");
+	trap.attack("skag");
+	trap.attack("skag");
+	trap.attack("skag");
+	trap.beRepaired(1);
 
 	std::cout << "\n--- COPY AND ASSIGNMENT ---" << std::endl;
 	ClapTrap original("Original");
@@ -50,11 +50,13 @@ int main(void)
 	std::cout << "\n--- Copy constructor ---" << std::endl;
 	ClapTrap copy(original);
 	copy.attack("a clone");
+	copy.beRepaired(1);
 
 	std::cout << "\n--- Copy assignment operator ---" << std::endl;
 	ClapTrap assigned("Assigned");
-	assigned = original;
+	assigned = copy;
 	assigned.attack("another clone");
+	assigned.beRepaired(1);
 
 	std::cout << "\n--- DESTRUCTORS ---" << std::endl;
 	return 0;
